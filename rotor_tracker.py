2ef19b00f7cb99b526c2b481b27095e9828491d5
rvig90 <rvig90@gmail.com>
2025-07-05 09:55:46 +0530

    Update rotor_tracker.py

--- rotor_tracker.py
+++ rotor_tracker.py
@@ -243,13 +243,13 @@ with st.expander("📋 View Movement Log", expanded=True):
 
         # 👉 Clear Filters button
            # 👉 Reset Filters Button
-        if st.button("🧹 Clear Filters"):
+         if st.button("🧹 Clear Filters"):
             st.session_state["sf"] = "All"
             st.session_state["pf"] = "All"
             st.session_state["zf"] = []
             st.session_state["rs"] = ""
     # Reset date range to full range of data
-             min_date = pd.to_datetime(st.session_state.data['Date']).min()
+            min_date = pd.to_datetime(st.session_state.data['Date']).min()
             max_date = pd.to_datetime(st.session_state.data['Date']).max()
             st.session_state["dr"] = [min_date, max_date]
             st.rerun() 
