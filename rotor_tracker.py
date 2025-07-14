426417a30afd92f2b4a42c66613bbd19700b4300
rvig90 <rvig90@gmail.com>
2025-07-14 20:56:37 +0530

    Update rotor_tracker.py

--- rotor_tracker.py
+++ rotor_tracker.py
@@ -287,7 +287,7 @@ with tabs[0]:
     st.subheader("🔮 Forecasted Rotor Demand (Next 6 Months)")
     
     available_sizes = sorted(outgoing["Size (mm)"].unique())
-     selected_size = st.selectbox("Select Rotor Size to Forecast", available_sizes)
+    selected_size = st.selectbox("Select Rotor Size to Forecast", available_sizes)
     
     # Filter data for selected size
     df_size = outgoing[outgoing["Size (mm)"] == selected_size]
