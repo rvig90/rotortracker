9725a21e1c2a7ec7b54e566c15a35a1430fea5a3
rvig90 <rvig90@gmail.com>
2025-07-29 12:52:01 +0530

    Update rotor_tracker.py

--- rotor_tracker.py
+++ rotor_tracker.py
@@ -236,7 +236,7 @@ with form_tabs[0]:
                         df = df.drop(future_matches.index)
                         st.success("🗑 Deleted future inward entry.")
 
-                       confirm_action = st.button("Save Changes")
+                    confirm_action = st.button("Save Changes")
                     if confirm_action:
                         if action == "Delete the future entry":
                             df = df.drop(index=selected_entry)
