1c08c6e6f5f88111fd13bcc37fb1f4668ca4fdf2
rvig90 <rvig90@gmail.com>
2025-08-09 00:05:43 +0530

    Update rotor_tracker.py

--- rotor_tracker.py
+++ rotor_tracker.py
@@ -1225,8 +1225,7 @@ if st.session_state["stator_data"].empty:
     st.session_state["stator_data"] = load_from_sheet("Stator Usage", st.session_state["stator_data"].columns)
 
 
-
-tab_choice == "🧰 Clitting + Laminations + Stators":
+tab_choice == ("🧰 Clitting + Laminations + Stators"):
     st.title("🧰 Clitting + Laminations + Stator Outgoings")
 
     CLITTING_USAGE = {
