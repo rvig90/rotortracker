2c94181f7cd0879d1004a3fced9372abf1b1b7ae
rvig90 <rvig90@gmail.com>
2026-02-20 18:19:25 +0530

    Update rotor_tracker.py

--- rotor_tracker.py
+++ rotor_tracker.py
@@ -690,7 +690,7 @@ if tab_choice == "🔁 Rotor Tracker":
     import streamlit as st
     import pandas as pd
     import numpy as np
-    
+    import plotly.express as px
     import plotly.graph_objects as go
     from datetime import datetime, timedelta
     import requests
